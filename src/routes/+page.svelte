10caad8a00a53835c066b3673d603fdeb5d34648
MrNamazu <92030086+MrNamazu@users.noreply.github.com>
2023-01-30 11:22:32 +0100

    2 files modified

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,61 @@
 <script>
 
 </script>
-<div class="">
-.
+<div class="flex flex-col gap-3">
+  <div class="cardBG p-3 flex gap-3">
+    <div class="w-32 h-32 rounded border-2 border-primary-main">
+      <img class="w-full" src="https://xivapi.com//i/032000/032394_hr1.png" alt="">
+    </div>
+    <div class="flex-1 flex gap-1 flex-col">
+      <p class="cardBG bg-secondary-900 px-3 py-2 text-lg font-semibold">Edenchoir coat of fending</p>
+      <p class="cardBG bg-secondary-900 px-3 py-1 text-xs font-semibold">Item level: 25</p>
+      <div class="flex justify-between">
+        <div class="relative w-20 h-8">
+          <p class="absolute text-[8px]">Auto Atk.</p>
+          <p class="absolute bottom-0 right-0 text-[12px] font-semibold">100.01</p>
+          <div class="absolute bottom-0 w-full h-1 bg-primary-main"></div>
+        </div>
+        <div class="relative w-20 h-8">
+          <p class="absolute text-[8px]">Auto Atk.</p>
+          <p class="absolute bottom-0 right-0 text-[12px] font-semibold">100.01</p>
+          <div class="absolute bottom-0 w-full h-1 bg-primary-main"></div>
+        </div>
+        <div class="relative w-20 h-8">
+          <p class="absolute text-[8px]">Auto Atk.</p>
+          <p class="absolute bottom-0 right-0 text-[12px] font-semibold">100.01</p>
+          <div class="absolute bottom-0 w-full h-1 bg-primary-main"></div>
+        </div>
+      </div>
+    </div>
+    <div class="cardBG bg-secondary-900 p-3 flex flex-col w-44 justify-between">
+      <div class="flex justify-between">
+        <p class="text-xs font-semibold">Selling Price:</p>
+        <p class="text-xs">2.000 Gil</p>
+      </div>
+      <div class="flex justify-between">
+        <p class="text-xs font-semibold">Selling Price:</p>
+        <p class="text-xs">2.000 Gil</p>
+      </div>
+      <div class="flex justify-between">
+        <p class="text-xs font-semibold">Selling Price:</p>
+        <p class="text-xs">2.000 Gil</p>
+      </div>
+      <div class="flex justify-between">
+        <p class="text-xs font-semibold">Selling Price:</p>
+        <p class="text-xs">2.000 Gil</p>
+      </div>
+      <div class="flex justify-between">
+        <p class="text-xs font-semibold">Selling Price:</p>
+        <p class="text-xs">2.000 Gil</p>
+      </div>
+      <div class="flex justify-between">
+        <p class="text-xs font-semibold">Selling Price:</p>
+        <p class="text-xs">2.000 Gil</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="cardBG p-3">
+
+  </div>
 </div>
